6fc309e391d1b8d314794c352608097393d41c3a
healthyCoder <36478181+healthyCoder@users.noreply.github.com>
2019-01-22 12:02:32 +0530

    Update 1
    Changelog:
    -Updated the icon
    -Updated the splash screen
    -Added two new components MainComponent.js and Timetable.js
    -Added navigation between app.js and MainComponent.js
    -Added line to gitignore to ignore VS Code files
    -Added react native elements
    -Added react navigation
    
    TODO:
    -Move contents of app component to another file and code login screen into app component
    -Add more tabs to the navigation bar
    -Complete timetable component.

--- timetable/App.js
+++ timetable/App.js
@@ -1,15 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation'
+import Icon from 'react-native-vector-icons/Ionicons'
 
-export default class App extends React.Component {
+import Main from './components/MainComponent';
+
+
+class App extends Component {
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text>This will be our root page. Start adding components over here.</Text>
+        <Text>This is App Component</Text>
+        {/* <TouchableOpacity><Text>Login</Text></TouchableOpacity> */}
+        {/* <Button onPress={() => this.props.navigation.navigate('Main')} title="Go to Main"></Button> */}
       </View>
     );
   }
 }
+
+
+export default createBottomTabNavigator({
+  home: { 
+    screen:App,
+    navigationOptions:{
+      tabBarLabel:'Home',
+      tabBarIcon : ({ tintColor }) => (
+        <Icon name="ios-home" size={24}/>
+      )
+    }
+  },
+
+  test: {
+    screen:Main,
+    navigationOptions:{
+      tabBarLabel:'Substitutions',
+      tabBarIcon : ({ tintColor }) => (
+        <Icon name="ios-settings" size={24}/>
+      )
+    }
+  }
+})
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ timetable/components/Timetable.js
@@ -0,0 +1,19 @@
+import React, { Components } from 'react'
+import{
+    View
+} from 'react-native'
+
+class Timetable extends Components{
+    
+    constructor(props){
+        super()
+    }
+
+    render(){
+        return{
+
+        };
+    }
+}
+
+export default Timetable
