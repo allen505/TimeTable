ab22def8a3521ffc8e2f001c1485f1aaadbc77eb
Abbaskt <42604146+Abbaskt@users.noreply.github.com>
2019-02-05 00:25:43 +0530

    Added the display part of the teachers

---
+++ Substitution.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react'
+import{
+    View,
+    ScrollView,
+    Text,
+    Button,
+    Alert,
+    Image
+} from 'react-native'
+import { createStackNavigator} from 'react-navigation';
+import TtView from './TtView'
+import DateTimePickerTester from './timePicker'
+import Status from './Status'
+import Cards from './Cards.js'
+
+import styles from '../style'
+
+class Substitution extends Component{
+ static navigationOptions = {
+        title: 'Pick a class',
+   
+    }
+    
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        const {container} = styles
+        return(  
+
+      
+            <ScrollView style ={container}>
+        <DateTimePickerTester />
+        <TtView />
+             <Button
+        title="Next"
+        onPress={() => this.props.navigation.navigate('Substitution2')}
+        />
+            </ScrollView>
+        );
+    }
+}
+
+_okfunc = () =>{
+    console.log('OK Pressed!');
+    <Status />
+};
+
+class Substitution2 extends Component{
+      static navigationOptions = {
+        title: 'Pick a teacher',
+        headerRight: (
+      <Button
+        onPress={() => Alert.alert(
+          'Confirmation',
+          'Do you wish to substitute the selected classes and notify the teacher?',
+          [
+            {text: 'Cancel', onPress: () => console.log('Cancel Pressed!')},
+            {text: 'OK', onPress: () => _okfunc()},
+          ],
+          { cancelable: false }
+        )}
+        title="Confirm"
+         />
+    )
+    }
+ 
+
+    
+    constructor(props){
+        super(props);
+    }
+
+
+    render(){
+        const {container,display} = styles
+        return(
+             <ScrollView style={container}>
+             <View style={display}>
+             <Cards/>
+             <Cards/>
+             <Cards/>
+             </View>
+             <View style={display}>
+             <Cards/>
+             <Cards/>
+             <Cards/>
+             </View>
+            </ScrollView>
+        );
+    }
+}
+
+export default createStackNavigator({
+   home: Substitution,
+   Substitution2: Substitution2,
+});
+
+
+
