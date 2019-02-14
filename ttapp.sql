9cec12cc1eb2b8d26e18f5adb943b0c1200dc18c
studboii <42826824+studboii@users.noreply.github.com>
2019-02-14 11:03:17 +0530

    added on more class and labhandler
    UID will save an ID, right now it's saving the subject for convenience. 
    
    For EG: Ajay's UID is se
    even though he handles ooc for class 1b, se will show up in the table.
    similar;y sarala's UID is mplab
    even though she handles se for 1b mplab will show

--- ttapp.sql
+++ ttapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 13, 2019 at 06:33 PM
+-- Generation Time: Feb 14, 2019 at 06:26 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -43,13 +43,42 @@ CREATE TABLE `class1a` (
 --
 
 INSERT INTO `class1a` (`Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`, `Period`) VALUES
-('hrd', 'cip', 'occ', 'm4', 'labs', 'se', 1),
-('hrd', 'daa', 'daa', 'daa', 'lab', 'daa', 2),
-('mp', 'ooc', 'se', 'mp', 'lab', 'dc', 3),
+('hrd', 'cip', 'occ', 'm4', 'lab1a', 'se', 1),
+('hrd', 'daa', 'daa', 'daa', 'lab1a', 'daa', 2),
+('mp', 'ooc', 'se', 'mp', 'lab1a', 'dc', 3),
 ('m4', 'dc', 'dc', 'dc', 'm4', 'se', 4),
-('lab', 'm4', NULL, 'lab', 'ooc', NULL, 5),
-('lab', 'm4', NULL, 'lab', 'ooc', NULL, 6),
-('lab', 'ooc', NULL, 'lab', 'mp', NULL, 7);
+('lab1a', 'm4', NULL, 'lab1a', 'ooc', NULL, 5),
+('lab1a', 'm4', NULL, 'lab1a', 'ooc', NULL, 6),
+('lab1a', 'ooc', NULL, 'lab1a', 'mp', NULL, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class1b`
+--
+
+CREATE TABLE `class1b` (
+  `Monday` varchar(50) DEFAULT NULL,
+  `Tuesday` varchar(50) DEFAULT NULL,
+  `Wednesday` varchar(50) DEFAULT NULL,
+  `Thursday` varchar(50) DEFAULT NULL,
+  `Friday` varchar(50) DEFAULT NULL,
+  `Saturday` varchar(50) DEFAULT NULL,
+  `Period` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `class1b`
+--
+
+INSERT INTO `class1b` (`Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`, `Period`) VALUES
+('dc_2', 'hrd', 'se', 'm4_2', 'mp_2', 'dc_2', 1),
+('mplab', 'hrd', 'dc_2', 'dc_2', 'se', 'cip', 2),
+('daa_2', 'se', 'mplab', 'mp_2', 'mplab', 'daa_2', 3),
+('m4_2', 'mplab', 'mp_2', 'ooc_2', 'daa_2', 'm4_2', 4),
+('lab1b', 'daa_2', NULL, 'lab1b', 'lab1b', NULL, 5),
+('lab1b', 'm4_2', NULL, 'lab1b', 'lab1b', NULL, 6),
+('lab1b', 'mp_2', NULL, 'lab1b', 'lab1b', NULL, 7);
 
 -- --------------------------------------------------------
 
@@ -80,6 +109,28 @@ INSERT INTO `classhandler` (`uid`, `class`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `labhandler`
+--
+
+CREATE TABLE `labhandler` (
+  `LabName` varchar(50) NOT NULL,
+  `uid` varchar(50) NOT NULL,
+  `labClass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='This table stores the UID of a teacher handling a particular lab for a class';
+
+--
+-- Dumping data for table `labhandler`
+--
+
+INSERT INTO `labhandler` (`LabName`, `uid`, `labClass`) VALUES
+('design', 'daalab', 'lab1a'),
+('design', 'daalab2', 'lab2b'),
+('microP', 'mplab', 'lab1a'),
+('microP', 'mplab2', 'lab2b');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `teachprof`
 --
 
@@ -89,6 +140,24 @@ CREATE TABLE `teachprof` (
   `dept` varchar(50) NOT NULL,
   `uid` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `teachprof`
+--
+
+INSERT INTO `teachprof` (`name`, `desg`, `dept`, `uid`) VALUES
+('Neethu Patil', 'Asst Prof', 'cse', 'cip'),
+('Dr Krishna A N', 'HOD', 'cse', 'DAA'),
+('Kala C', 'Asst Prof', 'cse', 'daalab'),
+('Dr SKC', 'Asst Prof', 'cse', 'daa_2'),
+('Roopashree', 'Asst Prof', 'cse', 'dc'),
+('Poornima H', 'Asst Prof', 'cse', 'm4'),
+('Geetha', 'Asst Prof', 'cse', 'm4_2'),
+('Mohan Kumar', 'Asst Prof', 'cse', 'MP'),
+('Sarala', 'Asst Prof', 'cse', 'mplab'),
+('Dr MS', 'Asst Prof', 'cse', 'mp_2'),
+('Rajat', 'Asst Prof', 'cse', 'ooc'),
+('Ajay BN', 'Asst Prof', 'cse', 'se');
 
 --
 -- Indexes for dumped tables
@@ -101,9 +170,21 @@ ALTER TABLE `class1a`
   ADD PRIMARY KEY (`Period`);
 
 --
+-- Indexes for table `class1b`
+--
+ALTER TABLE `class1b`
+  ADD PRIMARY KEY (`Period`);
+
+--
 -- Indexes for table `classhandler`
 --
 ALTER TABLE `classhandler`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- Indexes for table `labhandler`
+--
+ALTER TABLE `labhandler`
   ADD PRIMARY KEY (`uid`);
 
 --
@@ -120,6 +201,12 @@ ALTER TABLE `teachprof`
 -- AUTO_INCREMENT for table `class1a`
 --
 ALTER TABLE `class1a`
+  MODIFY `Period` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `class1b`
+--
+ALTER TABLE `class1b`
   MODIFY `Period` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
