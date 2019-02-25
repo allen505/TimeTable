fb075c32b1a9dd513b6159cf922f8d440079e70b
healthyCoder <36478181+healthyCoder@users.noreply.github.com>
2019-02-25 22:22:46 +0530

    ttView is finalised
    The ttview is correctly showing the data now.
    Final touch ups need to be done such as padding and margins

--- timetable/components/TtView.js
+++ timetable/components/TtView.js
@@ -28,72 +28,61 @@ class TtView extends Component{
     render(){
         const {container, ttView,timetext} = styles
 
-        let col1 = ['8:30-9:30','9:30-10:30','10:30-10:45',
+        let Time = ['8:30-9:30','9:30-10:30','10:30-10:45',
             '10:45-11:45','11:45-12:45','12:45-1:30',
             '1:30-2:30','2:30-3:30','3:30-4:30'];
-        let col2 = ['Class','Class','Tea Break',
+        let Subjects = ['Free','Class','Tea Break',
             'Class','Free','Lunch Break',
             'Free','Class','Free'];
         let free = RegExp('Free');
         let Break = RegExp('.*Break');
         let rows = [];     
-        let i=0; 
-
-        col1.map((data,index) => {
-            
-
-
-        //for(let i=0 ; i<9 ; i++){
-			let cols = []
-			let key="t"+i;
-			cols.push(<Text style = {timetext} key={key}> {col1[i]} </Text>)
-            if( free.test(col2[i] ) == true ){
-				key="b"+i;
+        let i=0;
+        
+        for(let i=0 ; i<9 ; i++){
+            let cols = []
+            cols.push(<Text style={{flex:2}}> {Time[i]} </Text>)
+            if( free.test(Subjects[i] ) == true ){
+                key="b"+i;
                 cols.push(
                     <Button
                         disabled
-                        key= {key}
-                        title={col2[i]}
+                        title={Subjects[i]}
                         type="solid"
+                        style={{flex:5}}
                     />
                 )
             }
-            else if ( Break.test(col2[i] ) == true ) {
+            else if ( Break.test(Subjects[i] ) == true ) {
                 cols.push(
                     <Button
                         disabled
-                        key= {i}
-                        title={col2[i]}
+                        title={Subjects[i]}
                         type="outline"
                         icon={
                             <Icon name="ios-close-circle-outline" size={24}/>
                         }
+                        style={{flex:5}}
                     />
                 )
             }
             else{
                 cols.push(
                     <CheckBox
-                        key= {index}
-                        title={col2[index]}
-                        checked={this.state.selected[index]}
-                        onPress={() => this.onSelection(index)}
-                       // onPress={() => this.setState({ checked: false })}
+                        title={Subjects[i]}
+                        containerStyle={{flex:5}}
+                        checked={this.state.selected[i]}
+                        onPress={() => this.onSelection(i)}
+                        // onPress={() => this.setState({ checked: false })}
                     />
                 )
-			}
-        rows.push(<View key={i} style={styles.ttView}>{cols}</View>)
-            
-            i++;
+            }
+            rows[i]=<View style={{flexDirection: "row"}}>{cols}</View>;
         }
-        );
-        
 
         return(
             <View style={ttView}>
-
                 {rows}   
-            
             </View>
        );
    }
