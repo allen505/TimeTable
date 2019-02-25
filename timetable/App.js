88de25f08d1191a7753161fd88c836b42f8ccced
AB047 <32891133+AB047@users.noreply.github.com>
2019-02-26 00:07:29 +0530

    Update App.js
    Added a login page with a close to complete fetch api. Login button currently has no authentication and is meant so for debugging purposes. Image has to be added at the top end of the page but currently adding an Image tag breaks the page to a white blank screen.

--- timetable/App.js
+++ timetable/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { View, Button } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation'
+import { View, Text, TextInput } from 'react-native';
+import { 
+  createBottomTabNavigator,
+  createSwitchNavigator
+ } from 'react-navigation'
 import Icon from 'react-native-vector-icons/Ionicons'
-
+import {Alert, TouchableOpacity } from 'react-native';
+import { AsyncStorage } from 'react-native';
 
 import styles from './style'
 import Timetable from './components/Timetable'
@@ -10,36 +14,7 @@ import Substitution from './components/Substitution'
 import Status from './components/Status'
 import Account from './components/Account'
 
-
-class App extends Component {
-  static navigationOptions = {
-    title: 'Login',
-
-  };
-
-
-  login(){
-    console.log("login() called");
-    navigate("timetable");
-  }
-
-  render() {
-    
-
-    const {container} = styles
-    const {navigate} = this.props.navigation;
-    return (
-     <View>
-     
-        <Button 
-        title = "Login"
-        onPress={() => this.login()} ></Button>
-      </View>
-    );
-  }
-}
-
-export default createBottomTabNavigator({
+const BottomTabNavigator = createBottomTabNavigator({
   timetable: { 
     screen:Timetable,
     navigationOptions:{
@@ -80,3 +55,161 @@ export default createBottomTabNavigator({
     }
   }
 })
+
+class WelcomeScreen extends Component{
+
+  state = {
+    username: '',
+    password: '',
+    auth_token_state: '',
+    
+  } 
+     Login = async () => {
+       fetch('LOGIN DATA LINK', {
+         method: 'post',
+         headers: {
+           'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({
+           "provider": "username",
+           "data": {
+             "username": this.state.username,
+             "password": this.state.password
+           }
+         })
+       }).then((response) => response.json())
+         .then((res) => {
+           if (typeof (res.message) != "undefined") {
+             Alert.alert("Error", "Error: " + res.message);
+           }
+           else {
+             this.setState({ auth_token: res.auth_token });
+             AsyncStorage.setItem('token',res.auth_token);
+             hasToken  = 'true';
+             Alert.alert("Welcome", " You have succesfully logged in");
+           }
+         }).catch((error) => {
+           console.error(error);
+         });
+
+      
+
+       }
+  tempfunc() {
+
+    this.Login.bind(this);
+    this.setState = '1234';
+    this.props.navigation.navigate('bottomTab');
+
+
+        
+     }
+     render(){
+       //If auth token is not present
+       if (this.state.auth_token_state == '') {
+         return (
+           <View style = {{
+             flex: 2,
+             justifyContent: "flex-end",
+             alignItems: "center",
+           }}>
+       
+          
+             <TextInput
+               placeholder="Enter User name"
+               onChangeText={TextInputValue =>
+                 this.setState({ username: TextInputValue })}
+               underlineColorAndroid='transparent'
+               style={
+                 {
+                   textAlign: 'center',
+                   width: '90%',
+                   marginBottom: 50,
+                   height: 40,
+                   borderRadius: 5,
+                   fontSize: 20,
+                   backgroundColor: "#EEEEEE"
+                 }
+               }
+             />
+            
+             <TextInput
+               placeholder="Enter password"
+               onChangeText={TextInputValue =>
+                 this.setState({ password: TextInputValue })}
+               underlineColorAndroid='transparent'
+               secureTextEntry={true}
+               style={
+                 {
+                   textAlign: 'center',
+                   width: '90%',
+                   marginBottom: 50,
+                   height: 40,
+                   borderRadius: 5,
+                   fontSize: 20,
+                   backgroundColor: "#EEEEEE"
+                 }
+               }
+             />
+
+            
+
+
+              <View>
+             <TouchableOpacity onPress={() => this.tempfunc()}>
+               <View style={{
+                 height: 50, 
+                 width: "70%",
+                 backgroundColor:'#147efb', 
+                 justifyContent: 'center',
+                 alignItems: 'center',
+                 margin: 50,
+                 borderRadius: 100
+               }}>
+                 <Text style={{ 
+                   fontSize: 20,
+                   color: '#FFFFFF',
+                 }}>
+                   Login </Text>
+                   
+               </View>
+             </TouchableOpacity>
+  
+           </View>
+           </View>
+         );
+       }
+       else
+       {
+          return(
+       <View>
+            {this.props.navigation.navigate("bottomTab")}
+            </View>
+
+          );
+       }
+      }
+    }
+  
+    
+
+
+
+const SwitchNav = createSwitchNavigator({
+  welcome : { screen : WelcomeScreen},
+  bottomTab:{ screen : BottomTabNavigator}
+})
+
+class App extends Component {
+
+  render() {
+  
+    return (
+    <SwitchNav/>
+    );
+  }
+}
+export default App
+
+
+
