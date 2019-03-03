cba45785207652aed5c0a1985ae926f6df3a7c62
studboii <42826824+studboii@users.noreply.github.com>
2019-03-03 23:26:25 +0530

    The API works now. Sorta
    Instructions on how to use it are documented. It's got code for retrieving a particular teacher's timetable which works.
    
    I am populating the db right now and will finish the other queries by tomorrow afternoon for the basic requests

--- restDraft.js
+++ restDraft.js
@@ -3,30 +3,37 @@
  const mysql = require('mysql');//object to acess mySQL properties
 
  const app =  express();// its a service to help build our api
- const IpQuery = "SELECT * FROM ajay";//this is my query, table exists and it works fine
+ var IpQuery = "SELECT * FROM ";//this is my query, table exists and it works fine
 
+ //var self = this;
  const conn = mysql.createConnection({
- 	host: 'localhost',
+ 	host: '127.0.0.1',
  	username: 'root',
+ 	user:'root',
  	password:'',
  	database: 'timetable'
- 	 });//defining an object to create a connection with
+
+ 	 });//defining an object to create a connection with i'll make this a pool later if it's more of an advantage and use the SSL port over https 
  conn.connect(err => {
  	if(err){
  		return err;
  	}
+
+ 	
  });//making a connection so you can use it #BItCoOOOOOOneccccct
- //console.log("get rekt bich");//for lulz and also to see if it works		
- 
- //console.log(conn); //should op connection was here to make sure it was connected to the right thing 
+ console.log(conn); //should op connection was here to make sure it was connected to the right thing 
  app.use(cors());//i dont know why we need this
 
 app.get('/',(req,res) => {
 	res.send('Hello from the Timetable Server');
+	
  });//this is a GET based request and opening localhost:4000 on your PC should get this to show up
 
-app.get('/teachTable',(req,res) => {
-	//res.send('lol k ur here');
+app.get('/teachTable/retrieve',(req,res) => {
+	const{teachId} = req.query;
+	//res.send(teachId);// in here to see what id was sent
+	IpQuery = IpQuery + teachId;
+	//res.send(IpQuery);//checking if teachID is being properly appended to IpQuery 
 	conn.query(IpQuery, (err, results) => {
 		if (err) {
 			return res.send(err);
@@ -37,7 +44,15 @@ app.get('/teachTable',(req,res) => {
 			})
 		}
 	})
- });
+});/*this is where you retrieve a particular teacher's timetable from. 
+For Eg: If you want a teacher whose id is "ajay", 
+your url would be
+"localhost:4000/teachtable/retrieve?teachId=ajay"
+
+the data is in the from of rows where each row is an object and all rows are stored in an array heded by 'data'.
+
+the rest of the querying should be simple enough...i think*/
+
 
  
 
